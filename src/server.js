13480394ba93e057b8208ca28eacee854f59a1ee
NicholasPaniccioli <njp7745@g.rit.edu>
2021-02-26 16:16:25 -0500

    copied src files from assignment two to here for a nice base to start and added the node

---
+++ src/server.js
@@ -0,0 +1,37 @@
+const http = require('http');
+const url = require('url');
+const query = require('querystring');
+const htmlHandler = require('./htmlResponses.js');
+const jsonHandler = require('./jsonResponses.js');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const urlStruct = {
+  '/': htmlHandler.getIndex,
+  '/success': jsonHandler.success,
+  '/badRequest': jsonHandler.badRequest,
+  '/unauthorized': jsonHandler.unauthorized,
+  '/forbidden': jsonHandler.forbidden,
+  '/internal': jsonHandler.internal,
+  '/notImplemented': jsonHandler.notImplemented,
+  '/notFound': jsonHandler.notFound,
+  notFound: jsonHandler.notFound,
+  '/style.css': htmlHandler.getStyle,
+};
+
+const onRequest = (request, response) => {
+  const parsedUrl = url.parse(request.url);
+  const params = query.parse(parsedUrl.query);
+  const acceptedTypes = request.headers.accept.split(',');
+
+  if (urlStruct[parsedUrl.pathname]) {
+    urlStruct[parsedUrl.pathname](request, response, acceptedTypes, params);
+    // urlStruct[parsedUrl.pathname](request, response, params);
+  } else {
+    urlStruct.notFound(request, response, params);
+  }
+};
+
+http.createServer(onRequest).listen(port);
+
+console.log(`Listening on 127.0.0.1; ${port}`);
